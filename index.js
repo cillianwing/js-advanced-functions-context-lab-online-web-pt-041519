61a0c3f9927dcdf72ff994f03fc950f7f429e5e3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-11-29 20:47:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ function calculatePayroll() {
   
 }
 
+function findEmployeeByFirstName() {
+  
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
